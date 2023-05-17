8a0304b8e6ef40d8dc1d56e2893d09b26d0e9c8b
mnshrm <asysnsrs665@gmail.com>
2023-05-17 19:22:04 +0530

    Add topbar

--- src/Scenes/global/Topbar.jsx
+++ src/Scenes/global/Topbar.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import React, { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  DarkModeOutlined,
+  LightModeOutlined,
+  NotificationsOutlined,
+  PersonOutline,
+  SettingsOutlined,
+} from "@mui/icons-material";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  const theme = useTheme();
+  const color = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box display="flex" justifyContent={"space-between"} p={2}>
+      {/* Search bar with Icon */}
+      <Box
+        display={"flex"}
+        backgroundColor={color.primary[400]}
+        borderRadius={"3px"}
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* Icon buttons */}
+      <Box display={"flex"}>
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "light" ? (
+            <LightModeOutlined />
+          ) : (
+            <DarkModeOutlined />
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlined />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+        <IconButton>
+          <PersonOutline />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Topbar;
