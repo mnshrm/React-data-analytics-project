3e77abe3e9282e334ddea4a95c83f6186eaa7aa1
mnshrm <asysnsrs665@gmail.com>
2023-06-02 21:10:04 +0530

    add bar chart

--- src/Scenes/Bar/Bar.jsx
+++ src/Scenes/Bar/Bar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Header from "../../Components/Header";
+import { Box } from "@mui/material";
+import BarChart from "../../Components/BarChart";
 const Bar = () => {
-  return <div>Bar</div>;
+  return (
+    <Box m="20px">
+      <Header title="Bar chart" subtitle="Simple Bar Chart" />
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default Bar;
