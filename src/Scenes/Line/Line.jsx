46e65ad2d627052f165c6bb4578c858d80ea2025
mnshrm <asysnsrs665@gmail.com>
2023-06-04 12:33:34 +0530

    Add line chart

--- src/Scenes/Line/Line.jsx
+++ src/Scenes/Line/Line.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "../../Components/Header";
+import { Box } from "@mui/material";
+import LineChart from "../../Components/LineChart";
 
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <Box m="20px">
+      <Header title="Line chart" subtitle="Simple line Chart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default Line;
