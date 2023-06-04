76e0ea5fbb1722feb0a01372a2e6c5c92a2ee0a0
mnshrm <asysnsrs665@gmail.com>
2023-06-04 13:07:52 +0530

    remove visual bugs

--- src/Scenes/Geography/Geography.jsx
+++ src/Scenes/Geography/Geography.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "../../Components/Header";
 import { Box } from "@mui/material";
 import Choropleth from "../../Components/Choropleth";
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 
 const Line = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Header title="Choropleth chart" subtitle="Simple choropleth chart" />
-      <Box height="75vh">
+      <Box height="75vh" border={`1px solid ${colors.grey[100]}`}>
         <Choropleth />
       </Box>
     </Box>
